03c464bed75949ef310a31b4a30c3f593508d140
Sathya <svakacharla@sapient.com>
2019-04-04 19:53:57 +0530

    svg icons added, sprite image created, css loader added, styles for icon added, display icon from sprite image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const $ = require("jquery");
-
+require('./style.css');
 $("#target").html("Hello Sathya Vakacharla");
+
+function requireAll(r) {
+  r.keys().forEach(r);
+}
+
+requireAll(require.context('../icons/', true, /\.svg$/));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
 const path = require("path");
-
+const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
+const webpack = require('webpack');
 module.exports = {
   entry:"./src/index.js",
   output:{
     filename:"main.js",
     path:path.resolve(__dirname, "dist")
-  }
+  },
+  // module: {
+  //   loaders: [{
+  //     test: /icons\/.*\.svg$/,
+  //     loader: 'svg-sprite-loader',
+  //     options: {
+  //       extract: true,
+  //       spriteFilename: './dist/img/icons.svg',
+  //       runtimeCompat: true
+  //     }
+  //   }]
+  // },
+  // plugins: [
+  //   new SpriteLoaderPlugin({
+  //     plainSprite: true
+  //   })
+  // ]
+  module: {
+    rules: [
+      {
+        test: /icons\/.*\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+            options: {
+              extract: true,
+              spriteFilename: './img/icons.svg',
+              runtimeCompat: true
+            }
+          }
+        ]
+      },
+      {
+            test:/\.css$/,
+            loader:'style-loader!css-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new SpriteLoaderPlugin({
+        plainSprite: true
+      })
+]
 }
